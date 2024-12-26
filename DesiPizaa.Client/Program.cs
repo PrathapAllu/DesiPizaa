9066d453be555e7e1da92b8b72e763cc7ae89127
PrathapAllu <prathap.allu2616@gmail.com>
2024-12-26 03:05:24 -0800

    Desi Pizza customize pizza dialog added

--- DesiPizaa.Client/Program.cs
+++ DesiPizaa.Client/Program.cs
@@ -1,4 +1,5 @@
 using DesiPizaa.Client;
+using DesiPizza.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<OrderState>();
 
 await builder.Build().RunAsync();
